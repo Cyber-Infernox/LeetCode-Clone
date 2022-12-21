44dfb266dbd9d8a7b96a686072f1c0ba81282eb9
Cyber_Infernox <sayonkar@gmail.com>
2022-12-21 21:59:10 +0530

    Fixed

--- Carousel.js
+++ Carousel.js
@@ -1,13 +1,13 @@
 // console.log("This Carousel Tutorial");
 
-const pack = document.querySelector('.pic');
+const pack = document.querySelector(".pic");
 const pics = Array.from(pack.children);
 
 // console.log(pack);
 // console.log(pack.children);
 
-const nxt = document.querySelector('#right');
-const pre = document.querySelector('#left');
+const nxt = document.querySelector("#right");
+const pre = document.querySelector("#left");
 
 const Width = pics[0].getBoundingClientRect().width;
 // console.log(Width);
@@ -19,61 +19,61 @@ const Width = pics[0].getBoundingClientRect().width;
 
 // Using Loops
 pics.forEach((element, index) => {
-    element.style.left = Width * index + 'px';
-})
+  element.style.left = Width * index + "px";
+});
 
 // When I click left, move slides to the left
-nxt.addEventListener('click', e => {
-    const currSlide = pack.querySelector('.currSlide');
-    // console.log(currSlide);
-    const nxtSlide = currSlide.nextElementSibling;
-    const moveBy = nxtSlide.style.left;
-    // move to next slide
-    pack.style.transform = 'translateX(-' + moveBy + ')';
-    currSlide.classList.remove('currSlide');
-    nxtSlide.classList.add('currSlide');
-})
+nxt.addEventListener("click", (e) => {
+  const currSlide = pack.querySelector(".currSlide");
+  // console.log(currSlide);
+  const nxtSlide = currSlide.nextElementSibling;
+  const moveBy = nxtSlide.style.left;
+  // move to next slide
+  pack.style.transform = "translateX(-" + moveBy + ")";
+  currSlide.classList.remove("currSlide");
+  nxtSlide.classList.add("currSlide");
+});
 // When I click right, move slides to the right
-pre.addEventListener('click', e => {
-    const currSlide = pack.querySelector('.currSlide');
-    // console.log(currSlide);
-    const nxtSlide = currSlide.previousElementSibling;
-    const moveBy = nxtSlide.style.left;
-    // move to next slide
-    pack.style.transform = 'translateX(-' + moveBy + ')';
-    currSlide.classList.remove('currSlide');
-    nxtSlide.classList.add('currSlide');
-})
+pre.addEventListener("click", (e) => {
+  const currSlide = pack.querySelector(".currSlide");
+  // console.log(currSlide);
+  const nxtSlide = currSlide.previousElementSibling;
+  const moveBy = nxtSlide.style.left;
+  // move to next slide
+  pack.style.transform = "translateX(-" + moveBy + ")";
+  currSlide.classList.remove("currSlide");
+  nxtSlide.classList.add("currSlide");
+});
 
 // Second Part
-const pack1 = document.querySelector('.pic1');
+const pack1 = document.querySelector(".pic1");
 const pics1 = Array.from(pack1.children);
 
-const nxt1 = document.querySelector('#righte');
-const pre2 = document.querySelector('#lefte');
+const nxt1 = document.querySelector("#righte");
+const pre2 = document.querySelector("#lefte");
 
 const Width1 = pics[0].getBoundingClientRect().width;
 
 pics1.forEach((element, index) => {
-    element.style.left = Width1 * index + 'px';
-})
+  element.style.left = Width1 * index + "px";
+});
 
-nxt1.addEventListener('click', e => {
-    const currSlide = pack.querySelector('.currSlide');
-    const nxtSlide = currSlide.nextElementSibling;
-    const moveBy = nxtSlide.style.left;
-    // move to next slide
-    pack1.style.transform = 'translateX(-' + moveBy + ')';
-    currSlide.classList.remove('currSlide');
-    nxtSlide.classList.add('currSlide');
-})
+nxt1.addEventListener("click", (e) => {
+  const currSlide = pack.querySelector(".currSlide");
+  const nxtSlide = currSlide.nextElementSibling;
+  const moveBy = nxtSlide.style.left;
+  // move to next slide
+  pack1.style.transform = "translateX(-" + moveBy + ")";
+  currSlide.classList.remove("currSlide");
+  nxtSlide.classList.add("currSlide");
+});
 // When I click right, move slides to the right
-pre2.addEventListener('click', e => {
-    const currSlide = pack.querySelector('.currSlide');
-    const nxtSlide = currSlide.previousElementSibling;
-    const moveBy = nxtSlide.style.left;
-    // move to next slide
-    pack1.style.transform = 'translateX(-' + moveBy + ')';
-    currSlide.classList.remove('currSlide');
-    nxtSlide.classList.add('currSlide');
-})
+pre2.addEventListener("click", (e) => {
+  const currSlide = pack.querySelector(".currSlide");
+  const nxtSlide = currSlide.previousElementSibling;
+  const moveBy = nxtSlide.style.left;
+  // move to next slide
+  pack1.style.transform = "translateX(-" + moveBy + ")";
+  currSlide.classList.remove("currSlide");
+  nxtSlide.classList.add("currSlide");
+});
--- ProgressBar.js
+++ ProgressBar.js
@@ -1,29 +1,29 @@
 function updateProgressBar(progressBar, value) {
-    value = Math.round(value);
-    progressBar.querySelector(".progressBar").style.width = `${value}%`;
+  value = Math.round(value);
+  progressBar.querySelector(".progressBar").style.width = `${value}%`;
 }
 
 const pBar = document.querySelector(".prog");
 setTimeout(() => {
-    updateProgressBar(pBar, 77.8);
+  updateProgressBar(pBar, 77.8);
 }, 1000);
 
 function updateProgressBar1(progressBar1, value1) {
-    value1 = Math.round(value1);
-    progressBar1.querySelector(".progressBar1").style.width = `${value1}%`;
+  value1 = Math.round(value1);
+  progressBar1.querySelector(".progressBar1").style.width = `${value1}%`;
 }
 
 const pBar1 = document.querySelector(".prog1");
 setTimeout(() => {
-    updateProgressBar1(pBar1, 52.8);
+  updateProgressBar1(pBar1, 52.8);
 }, 1000);
 
 function updateProgressBar2(progressBar2, value2) {
-    value2 = Math.round(value2);
-    progressBar2.querySelector(".progressBar2").style.width = `${value2}%`;
+  value2 = Math.round(value2);
+  progressBar2.querySelector(".progressBar2").style.width = `${value2}%`;
 }
 
 const pBar2 = document.querySelector(".prog2");
 setTimeout(() => {
-    updateProgressBar2(pBar2, 46.8);
+  updateProgressBar2(pBar2, 46.8);
 }, 1000);
